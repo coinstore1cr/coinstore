678dc58c41b08ca231546389d33630fbe54f2243
coinstore1cr <coinstorect@gmail.com>
2025-02-27 01:09:50 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://82.29.164.124:3000/api'  // Your backend URL
+    apiUrl: 'https://82.29.164.124:3000/api'  // Your backend URL
     // Add other development-specific variables here
   };
