d3de9d8d0a5abe3c5130d60a7f3fee73d788c3fc
coinstore1cr <coinstorect@gmail.com>
2025-06-24 02:40:45 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://api.paxfulct.store/api'
     // Add other development-specific variables here
   };
